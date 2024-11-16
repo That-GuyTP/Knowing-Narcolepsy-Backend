540cf7880ee390b4a98f72d114cbd8aa08a07f21
Thomas Peterson <tap6@email.sc.edu>
2024-11-16 15:00:57 -0500

    Changed file organization for images

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./public/images/");
+        cb(null, "./public/images/people");
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
