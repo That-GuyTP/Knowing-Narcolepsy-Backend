fd39e0bcea5e3aa3a8338215d8accad3f3df5ae8
Thomas Peterson <tap6@email.sc.edu>
2024-11-16 15:05:56 -0500

    Changed file destination

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./public/images/people");
+        cb(null, "./public/images");
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
